1c9d586ea4fe523d2014dc240416ab906e9077a2
Lâm Quang Vinh <62162620+Vinhlam2512@users.noreply.github.com>
2023-03-05 10:39:31 +0700

    Vinhlq_05032023_1
    Fill data to fragment Course

--- app/src/main/java/com/example/myapplication/model/FragmentCourse.java
+++ app/src/main/java/com/example/myapplication/model/FragmentCourse.java
@@ -9,8 +9,11 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.myapplication.R;
+import com.example.myapplication.adapter.CourseAdapter;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -18,11 +21,19 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FragmentCourse extends Fragment {
+    private RecyclerView recyclerView;
+    private CourseAdapter adapter;
+    private DatabaseReference databaseRef;
+    List<CourseModel> courses = new ArrayList<>();
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_course, container, false);
+
         return view;
     }
 
@@ -32,14 +43,62 @@ public class FragmentCourse extends Fragment {
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference myRef = database.getReference("User");
 
+        recyclerView = view.findViewById(R.id.listCourse);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(requireContext());
+        recyclerView.setLayoutManager(layoutManager);
 
-        User u = new User();
-        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("User").child("-NP_eUZamDv2vX8DH3a4");
+        databaseRef = FirebaseDatabase.getInstance().getReference("Courses");
+        databaseRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                courses.clear();
 
-        databaseReference.child("users").child("-NP_eUZamDv2vX8DH3a4").getKey()
+                for (DataSnapshot courseSnapshot : dataSnapshot.getChildren()) {
+                    CourseModel course = courseSnapshot.getValue(CourseModel.class);
+                    courses.add(course);
+                }
+                adapter.notifyDataSetChanged();
+            }
 
-        TextView t = view.findViewById(R.id.text);
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                // Handle the error
+            }
+        });
 
-        t.setText(u.getFullName());
+        adapter = new CourseAdapter(requireContext(), courses);
+        recyclerView.setAdapter(adapter);
+
+//        User u = new User();
+//        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("User").child("-NP_eUZamDv2vX8DH3a4");
+//
+//        TextView t = view.findViewById(R.id.text);
+//
+//        t.setText(u.getFullName());
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // Add a listener for the Firebase database reference
+        databaseRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                courses.clear();
+
+                for (DataSnapshot courseSnapshot : dataSnapshot.getChildren()) {
+                    CourseModel course = courseSnapshot.getValue(CourseModel.class);
+                    courses.add(course);
+                }
+
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+            }
+        });
+    }
+
 }
