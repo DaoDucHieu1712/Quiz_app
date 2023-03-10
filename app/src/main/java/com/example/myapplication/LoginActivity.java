7ed19671f3e012cf7b0caddd0fbb95b94f704ba5
Chính Hoàng <hoangtche150003@fpt.edu.vn>
2023-03-10 16:43:57 +0700

    hoangtc_10032023_1
    screen home quizlet

--- app/src/main/java/com/example/myapplication/LoginActivity.java
+++ app/src/main/java/com/example/myapplication/LoginActivity.java
@@ -4,11 +4,10 @@ package com.example.myapplication;
 
 import android.content.Intent;
 import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
+//import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
-=======
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
