4b1eb8ca8b30290696fb446bcd4fd33895bab3f5
Chính Hoàng <hoangtche150003@fpt.edu.vn>
2023-03-11 14:47:20 +0700

    hoangtc_11032023_1
    done homepage and logout

--- app/src/main/java/com/example/myapplication/HomePageActivity.java
+++ app/src/main/java/com/example/myapplication/HomePageActivity.java
@@ -1,14 +1,45 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class HomePageActivity extends AppCompatActivity {
+    private CardView cv_logout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+        TextView tv_username = findViewById(R.id.tv_usernameHome);
+        cv_logout = findViewById(R.id.cv_logout);
+
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+
+        if (firebaseUser != null) {
+            String emailUser = firebaseUser.getEmail();
+            tv_username.setText("初めまして, " + emailUser + "!");
+            Toast.makeText(this, "Hello " + emailUser + "!", Toast.LENGTH_SHORT).show();
+        }
+
+        //logout
+        cv_logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Toast.makeText(HomePageActivity.this, "Logout successful, See u next time", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(HomePageActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
